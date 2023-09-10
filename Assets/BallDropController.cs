700037187831777d83d3960466bf44c6731dfa49
Beanie-beep <beanie.thehat@gmail.com>
2023-09-09 17:04:56 -0700

    Full Project
    Fresh start with GitHub

---
+++ Assets/BallDropController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BallDropController : MonoBehaviour
+{
+    public XRSocketInteractor socketInteractor; // Reference to the XR Socket Interactor
+    public Rigidbody AlumBall; // Reference to the ball's Rigidbody component
+    public Rigidbody CopperBall;
+    public Transform dropPoint; // The point where you want the ball to fall
+
+    
+
+    public float liquidDrag;
+
+    private bool dropBall = false; // Flag to track if the ball is attached to the controller
+
+    
+
+    private void Start()
+    {
+        // Make sure the socket interactor is not null
+        if (socketInteractor == null)
+        {
+            Debug.LogError("Socket Interactor not assigned!");
+            enabled = false;
+            return;
+        }
+
+        
+    }
+    
+
+
+
+
+    public void DropBall()
+    {
+        if (!dropBall)
+        {
+            dropBall = true;
+
+            //AlumBall.drag +=  liquidDrag;
+            //CopperBall.drag += liquidDrag;
+
+            socketInteractor.socketActive = false;
+            Debug.Log(socketInteractor.socketActive);
+
+            StartCoroutine(WaitForOneSecond());
+
+            
+
+        }
+        else
+        {
+            // Reset dropBall flag and re-enable socket interactor immediately
+            dropBall = false;
+            socketInteractor.socketActive = true;
+        }
+    }
+
+    IEnumerator WaitForOneSecond()
+    {
+        // Wait for 1 second
+        yield return new WaitForSeconds(5.0f);
+
+        // Code here will execute after waiting for 1 second
+        Debug.Log("One second has passed!");
+
+        socketInteractor.socketActive = true;
+        dropBall = false;
+        Debug.Log(socketInteractor.socketActive);
+    }
+}
---
+++ Assets/HandheldTimer.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class HandheldTimer : MonoBehaviour
+
+{
+    public TextMeshProUGUI text;
+
+    public BallDropController ballDropController; // Reference to the BallDropController script
+    public BallDropController ballDropControllerG;
+
+    public InputAction aButtonAction;
+
+    public XRGrabInteractable grabInteractable; //access stopwatch interactable
+
+
+    private bool countStarted; // Flag to track whether the countdown has started
+
+    public float time = 0f;
+    
+
+    private void OnEnable()
+    {
+        aButtonAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        aButtonAction.Disable();
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        countStarted = false;
+        
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.activated.AddListener(StartStopCountdown);
+
+        // Get the XRGrabInteractable component attached to this object
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        if (grabInteractable == null)
+        {
+            Debug.LogError("This object does not have an XRGrabInteractable component.");
+        }
+
+    }
+
+    private void Update()
+    {
+        if (countStarted)
+        {
+            time += Time.deltaTime;
+            //Debug.Log(time);
+            //text.text = "00:" + time + "s
+            ShowTime();
+            
+            ballDropController.DropBall();
+            ballDropControllerG.DropBall();
+
+        }
+
+        if(!countStarted)
+        {
+            
+            //Debug.Log(time);
+        }
+
+        if (aButtonAction.ReadValue<float>() > 0.5f && grabInteractable.isSelected)
+        {
+            Debug.Log("A button pressed!");
+            // Your code here to respond to the A button press
+            ResetTimer();
+        }
+
+
+    }
+
+    public void StartStopCountdown(ActivateEventArgs arg)
+    {
+        if(!countStarted) { countStarted = true; }
+        else if(countStarted) { countStarted = false; }
+    }
+
+    public void ResetTimer()
+    {
+        time = 0f;
+        Debug.Log(time);
+        //text.text = "00:" + time + "s";
+        ShowTime();
+    }
+
+    private void ShowTime()
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
+
+        string timeText = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds / 10);
+
+
+        text.text = timeText;
+    }
+}
